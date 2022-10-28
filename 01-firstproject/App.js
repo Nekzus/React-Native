7c6e007e86f9c6e724a5fe9ef46063bfedddf06e
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-10-28 19:20:28 -0300

    Primer commit proyectos

---
+++ 01-firstproject/App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hola, Coder!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 40,
+  },
+});
