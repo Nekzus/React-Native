a02013012b619f815d6dab4ea009dac3024c0f54
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-11-05 13:53:23 -0300

    Trabajo ejercicio 2

--- 02-secondproject/App.js
+++ 02-secondproject/App.js
@@ -1,28 +1,57 @@
-import { Button, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { styles } from "./styles";
+import { useState } from "react";
 
 export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [itemList, setItemList] = useState([]);
+
+  const onHandlerChangeItem = (t) => setTextItem(t);
+
+  const onHandlerDelete = () => null;
+
+  const addItem = () => {
+    setItemList((currentItems) => [
+      ...currentItems,
+      { id: Math.random().toString(), value: textItem },
+    ]);
+    setTextItem("");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Input todo" style={styles.input} />
-        <Button title="Add" color="#9a848f" onPress={() => null} />
+        <TextInput
+          placeholder="Input todo"
+          onChangeText={onHandlerChangeItem}
+          value={textItem}
+          style={styles.input}
+        />
+        <Button title="Add" color="#9a848f" onPress={addItem} />
       </View>
       <View style={styles.listContainer}>
         <Text style={styles.listTitle}>Todo List</Text>
-        <View style={styles.listItemContainer}>
-          <Text style={styles.listItem}>Todo 1</Text>
-        </View>
-        <View style={styles.listItemContainer}>
-          <Text style={styles.listItem}>Todo 2</Text>
-        </View>
-        <View style={styles.listItemContainer}>
-          <Text style={styles.listItem}>Todo 3</Text>
-        </View>
-        <View style={styles.listItemContainer}>
-          <Text style={styles.listItem}>Todo 4</Text>
-        </View>
+        <FlatList
+          data={itemList}
+          renderItem={(data) => (
+            <TouchableOpacity
+              onPress={onHandlerDelete.bind(this, data.item.id)}
+            >
+              <View style={styles.listItemContainer}>
+                <Text style={styles.listItem}>{data.item.value}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </View>
   );
