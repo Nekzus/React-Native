af1b1c3a9c24c47c3689fe49710b1ca09d3e058f
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-12-09 22:57:43 -0300

    test(Devtools): :bulb: Code adaptation for tests with status debug.
    Code adaptation for tests with status debugging with the redux tool.

--- 05-fifthproject/src/store/index.js
+++ 05-fifthproject/src/store/index.js
@@ -8,6 +8,12 @@ const rootReducer = combineReducers({
   team: teamReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+//TODO: Desarrollo
+const composeEnhancers =
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })) ||
+  compose;
 export default createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
